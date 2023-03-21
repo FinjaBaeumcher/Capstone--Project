3947ea3a643b51f894c08bf3cce2e688746dc58b
Finja Baeumcher <finja.baeumcher@googlemail.com>
2023-03-21 10:10:28 +0100

    add api

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import GlobalStyle from "../styles";
 import Head from "next/head";
+import useSWR from "swr";
+import { SWRConfig } from "swr";
 
 export default function App({ Component, pageProps }) {
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+  const { data } = useSWR("https://yoga-api-nzy4.onrender.com/v1", fetcher);
+
   return (
     <>
       <GlobalStyle />
