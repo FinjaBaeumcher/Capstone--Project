59625a0ed65276f7b7f6da80d73884699acf930a
Finja Baeumcher <finja.baeumcher@googlemail.com>
2023-03-22 14:24:10 +0100

    add three random Yoga practices

--- components/Random Practices/index.js
+++ components/Random Practices/index.js
@@ -1,9 +1,9 @@
 import Image from "next/image";
 
-export default function RandomPractice({ name, description, image, alt }) {
+export default function RandomPractice({ key, name, description, image, alt }) {
   return (
     <ul>
-      <li>
+      <li key={key}>
         <Image src={image} width={200} height={200} alt={alt} />
         <h2>{name}</h2>
         {description}
--- pages/index.js
+++ pages/index.js
@@ -9,19 +9,30 @@ export default function Home({ poses }) {
   const randomPractice = handleRandomPractice();
 
   function handleRandomPractice() {
-    const random = Math.floor(Math.random() * poses.length);
-    return random;
+    const random1 = Math.floor(Math.random() * poses.length);
+    let random2 = random1;
+    let random3 = random1;
+    while (random2 === random1) {
+      random2 = Math.floor(Math.random() * poses.length);
+    }
+    while (random3 === random1 || random3 === random2) {
+      random3 = Math.floor(Math.random() * poses.length);
+    }
+    return [random1, random2, random3];
   }
 
   return (
     <main>
       <Heading>Yoga App</Heading>
-      <RandomPractice
-        name={poses[randomPractice].english_name}
-        description={poses[randomPractice].pose_description}
-        image={poses[randomPractice].url_svg}
-        alt={poses[randomPractice].url_svg_alt}
-      />
+      {randomPractice.map((randomPractice) => (
+        <RandomPractice
+          key={poses[randomPractice].id}
+          name={poses[randomPractice].english_name}
+          description={poses[randomPractice].pose_description}
+          image={poses[randomPractice].url_svg}
+          alt={poses[randomPractice].url_svg_alt}
+        />
+      ))}
     </main>
   );
 }
