4635e7ceb6625557a4f54a348ece7f972891fed0
Finja Baeumcher <finja.baeumcher@googlemail.com>
2023-04-12 20:42:57 +0200

    add suggestions

--- components/FilteredPoses/index.js
+++ components/FilteredPoses/index.js
@@ -4,23 +4,10 @@ import { useState } from "react";
 import InputField from "../InputField";
 
 export default function FilteredPoses({ poses }) {
-  const [filter, setFilter] = useState("");
-
-  function handleSave(event) {
-    setFilter(event.target.value);
-  }
-
-  const filteredResults = poses.filter((pose) =>
-    pose.benefit.toLowerCase().includes(filter.toLowerCase())
-  );
-
   return (
     <>
-      <InputField value={filter} onChange={handleSave}>
-        Auf welchen Körperbereich möchtst du dich konzentrieren?
-      </InputField>
       <ul>
-        {filteredResults.map((pose) => (
+        {poses.map((pose) => (
           <li key={pose._id}>
             <Link href={`/flow/${pose._id}`}>
               <Image
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,59 @@
-import FilteredPoses from "../components/FilteredPoses";
+import Heading from "../components/Heading";
+import Link from "next/link";
+import { useState } from "react";
+import Router from "next/router";
 
 export default function Homepage({ poses }) {
+  const [filter, setFilter] = useState("");
+
+  function handleFilterChange(event) {
+    setFilter(event.target.value);
+  }
+
+  const filteredPoses = poses.filter((pose) =>
+    pose.benefit.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  function handleButtonClick() {
+    Router.push({
+      pathname: "/peak-pose",
+      query: { poses: JSON.stringify(filteredPoses) },
+    });
+  }
+
   return (
     <>
-      <FilteredPoses poses={poses} />
+      <Heading>Yoga App</Heading>
+      <form>
+        <label htmlFor="poseFilter">
+          Auf welchen Körperbereich möchtst du dich konzentrieren?
+        </label>
+        <input
+          list="filter"
+          value={filter}
+          onChange={handleFilterChange}
+          placeholder="Filter nach Körperteil"
+          name="poseFilter"
+          type="text"
+          id="poseFilter"
+        />
+        <datalist id="filter">
+          <>
+            <option value="Arme" />
+            <option value="Bauch" />
+            <option value="Beine" />
+            <option value="Brust" />
+            <option value="Hintern" />
+            <option value="Hüfte" />
+            <option value="Nacken-Schulter-Bereich" />
+            <option value="Rücken" />
+          </>
+        </datalist>
+      </form>
+      <button onClick={handleButtonClick}>Los</button>
+      <button>
+        <Link href="/random-peak-pose">Zufällig</Link>
+      </button>
     </>
   );
 }
