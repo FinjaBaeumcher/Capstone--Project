209b95720aff9355ef0908e4d9a40d4bc1182c15
Finja Baeumcher <finja.baeumcher@googlemail.com>
2023-04-14 11:20:50 +0200

    style flow description

--- components/AppropriateFlow/index.js
+++ components/AppropriateFlow/index.js
@@ -3,10 +3,16 @@ import styled from "styled-components";
 export default function AppropriateFlow({ poses, flow }) {
   const { name, description, time } = flow;
 
+  function formatText(text) {
+    return text.split(".").map((part, i) => {
+      return i % 2 === 0 ? <p key={i}>{part}</p> : <span key={i}>{part}</span>;
+    });
+  }
+
   return (
     <article key={poses._id}>
       <StyledHeading>{name}</StyledHeading>
-      <StyledDescription>{description}</StyledDescription>
+      <StyledDescription>{formatText(description)}</StyledDescription>
       <StyledTime>{time}</StyledTime>
     </article>
   );
@@ -16,19 +22,21 @@ const StyledTime = styled.p`
   margin: 10px;
   border: grey 1px solid;
   padding: 5px;
-  background-color: lavender;
+  background-color: mediumpurple;
   border-radius: 8px;
   font-size: 16px;
 `;
 
 const StyledHeading = styled.h2`
   color: purple;
-  margin: 10px;
+  padding: 10px;
   font-size: 24px;
+  text-align: center;
 `;
 
 const StyledDescription = styled.p`
   margin: 20px;
   font-size: 18px;
   color: dimgray;
+  text-align: center;
 `;
